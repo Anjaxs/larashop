70f754e1ec22a4dd93fbb6f3ed5ea2879346b17a
anjaxs <942403648@qq.com>
2020-12-08 21:27:42 +0800

    基础布局

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,17 @@
 <?php
 
-if (!function_exists('test_helper')) {
-    function test_helper() {
+use Illuminate\Support\Facades\Route;
+
+if (! function_exists('test_helper')) {
+    function test_helper()
+    {
         echo 'OK';
+    }
+}
+
+if (! function_exists('route_class')) {
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
     }
 }
