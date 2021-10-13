daac683a5e3b2f90a6ef8af3081416eb761f79a4
Anjaxs <942403648@qq.com>
2021-10-13 19:50:47 +0800

    修复没有引入ProductSku

--- app/Http/Controllers/Order/CartController.php
+++ app/Http/Controllers/Order/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product\ProductSku;
 use App\Services\Order\Cart\AddCart;
 use Illuminate\Http\Request;
 
