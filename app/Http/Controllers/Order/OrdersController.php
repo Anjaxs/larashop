8af0425c61849ee6a02a29cb9900b8bb10088315
Anjaxs <942403648@qq.com>
2021-10-25 20:40:30 +0800

    订单列表

--- app/Http/Controllers/Order/OrdersController.php
+++ app/Http/Controllers/Order/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Order;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order\Order;
 
 class OrdersController extends Controller
 {
@@ -15,5 +16,17 @@ class OrdersController extends Controller
         $order = app(\App\Services\Order\CreateOrder::class)->execute($data);
 
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
