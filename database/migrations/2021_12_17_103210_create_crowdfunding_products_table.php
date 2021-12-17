d78e87d9d19283fda6f893acbde0b35264f35a8c
Anjaxs <942403648@qq.com>
2021-12-17 11:33:53 +0800

    管理后台 - 商品控制器封装

--- database/migrations/2021_12_17_103210_create_crowdfunding_products_table.php
+++ database/migrations/2021_12_17_103210_create_crowdfunding_products_table.php
@@ -17,11 +17,11 @@ class CreateCrowdfundingProductsTable extends Migration
             $table->id();
             $table->unsignedInteger('product_id');
             // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->decimal('target_amount', 10, 2);
-            $table->decimal('total_amount', 10, 2)->default(0);
-            $table->unsignedInteger('user_count')->default(0);
-            $table->dateTime('end_at');
-            $table->string('status')->default(\App\Models\Product\CrowdfundingProduct::STATUS_FUNDING);
+            $table->decimal('target_amount', 10, 2)->comment('众筹目标金额');
+            $table->decimal('total_amount', 10, 2)->default(0)->comment('当前已筹金额');
+            $table->unsignedInteger('user_count')->default(0)->comment('当前参与众筹用户数');
+            $table->dateTime('end_at')->comment('众筹截止时间');
+            $table->string('status')->default(\App\Models\Product\CrowdfundingProduct::STATUS_FUNDING)->comment('当前筹款的状态');
         });
     }
 
