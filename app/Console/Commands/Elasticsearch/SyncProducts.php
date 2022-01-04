7e35adfad1aa0aecafc9dd262ed981f59972405e
Anjaxs <942403648@qq.com>
2022-01-04 16:15:51 +0800

    elasticsearch migrate

--- app/Console/Commands/Elasticsearch/SyncProducts.php
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -7,7 +7,7 @@ use Illuminate\Console\Command;
 
 class SyncProducts extends Command
 {
-    protected $signature = 'es:sync-products';
+    protected $signature = 'es:sync-products {--index=products}';
 
     protected $description = '将商品数据同步到 Elasticsearch';
 
@@ -20,8 +20,6 @@ class SyncProducts extends Command
     {
         /** @var \Elasticsearch\Client 对象*/
         $es = app('es');
-
-        $this->createProductsIndexIfNotExist($es);
 
         Product::query()
             // 预加载 SKU 和 商品属性数据，避免 N + 1 问题
@@ -39,7 +37,7 @@ class SyncProducts extends Command
 
                     $req['body'][] = [
                         'index' => [
-                            '_index' => 'products',
+                            '_index' => $this->option('index'),
                             '_id'    => $data['id'],
                         ],
                     ];
@@ -53,100 +51,5 @@ class SyncProducts extends Command
                 }
             });
         $this->info('同步完成');
-    }
-
-    /**
-     * 如果不存在 products index 则创建
-     * @param \Elasticsearch\Client $es
-     */
-    protected function createProductsIndexIfNotExist($es)
-    {
-        try {
-            $es->indices()->getMapping(['index' => 'products']);
-        } catch (\Elasticsearch\Common\Exceptions\Missing404Exception $e) {
-            $es->indices()->create([
-                'index' => 'products',
-                'body' => [
-                    'mappings' => [
-                        'properties'  => [
-                            "type" => [
-                                "type" => "keyword"
-                            ],
-                            "title" => [
-                                "type" => "text",
-                                "analyzer" => "ik_smart"
-                            ],
-                            "long_title" => [
-                                "type" => "text",
-                                "analyzer" => "ik_smart"
-                            ],
-                            "category_id" => [
-                                "type" => "integer"
-                            ],
-                            "category" => [
-                                "type" => "keyword"
-                            ],
-                            "category_path" => [
-                                "type" => "keyword"
-                            ],
-                            "description" => [
-                                "type" => "text",
-                                "analyzer" => "ik_smart"
-                            ],
-                            "price" => [
-                                "type" => "scaled_float",
-                                "scaling_factor" => 100
-                            ],
-                            "on_sale" => [
-                                "type" => "boolean"
-                            ],
-                            "rating" => [
-                                "type" => "float"
-                            ],
-                            "sold_count" => [
-                                "type" => "integer"
-                            ],
-                            "review_count" => [
-                                "type" => "integer"
-                            ],
-                            "skus" => [
-                                "type" => "nested",
-                                "properties" => [
-                                    "title" => [
-                                        "type" => "text",
-                                        "analyzer" => "ik_smart",
-                                        "copy_to" => "skus_title"
-                                    ],
-                                    "description" => [
-                                        "type" => "text",
-                                        "analyzer" => "ik_smart",
-                                        "copy_to" => "skus_description"
-                                    ],
-                                    "price" => [
-                                        "type" => "scaled_float",
-                                        "scaling_factor" => 100
-                                    ]
-                                ]
-                            ],
-                            "properties" => [
-                                "type" => "nested",
-                                "properties" => [
-                                    "name" => [
-                                        "type" => "keyword"
-                                    ],
-                                    "value" => [
-                                        "type" => "keyword",
-                                        "copy_to" => "properties_value"
-                                    ],
-                                    "search_value" => [
-                                        "type" => "keyword",
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]);
-        }
     }
 }
